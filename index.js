8159fd7b325e7903b07e9280e1d5c77fdf9df213
Arif Billah Babu <arif18bl@gmail.com>
2022-10-06 15:26:56 +0600

    index.js file created and mongodb connected

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/nodejsautocomplete')
+    .then(()=> console.log('MongoDB Connected Successfully'))
+    .catch(()=> console.error('Connection Failed!'));
+
+
+
+app.listen(8000, ()=>{
+    console.log("Server is Running!");
+});
+
+
+
